0e9eade02d74ac6818729c972c6a4b28226dd843
Victor Ericsson <victor@victors-mbp.students.netlogon.liu.se>
2022-11-10 18:16:46 +0100

    Inital work on the menu_state object

--- menu_state.h
+++ menu_state.h
@@ -2,6 +2,32 @@
 #define MENU_STATE.H
 
 #include <SFML/Graphics.hpp>
+#include <list>
+
+class Menu_state
+{
+
+    public:
+    Menu_state();
+
+    list<Tanks*> players;
+
+    Map map;
+
+    override void handle(sf::Event event);
+
+    override void update(sf::Time delta);
+
+    override void render(sf::RenderWindow& window);
+
+    ~Menu_state();
+
+    private:
+    void add_player();
+    void choose_map();
+   
+};
+
 
 
 
