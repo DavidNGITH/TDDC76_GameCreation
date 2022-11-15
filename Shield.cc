f8df3da581088d7b43a86f5233a48d3132eb04e4
matwe073 <matwe073@tlvm-4-4-3.ad.liu.se>
2022-11-15 15:32:26 +0100

    Startup for the classes Shield and Repair

---
+++ Shield.cc
@@ -0,0 +1,52 @@
+#include "SFML/Graphics.hpp"
+
+#include "game_state.h"
+
+Shield::Shield()
+{
+
+}
+
+
+void Shield::handle(Context& context, sf::Event event)
+{
+
+}
+
+void Shield::update(Context& context)
+{
+
+}
+
+void Shield::render(sf::RenderWindow& window)
+{
+
+}
+
+
+void Shield::collision()
+{
+
+}
+
+void Shield::move()
+{
+
+}
+
+void Shield::remove()
+{
+
+}
+
+bool Shield::is_removed()
+{
+
+}
+        
+        
+        
+bool Shield::add_shield()
+{
+
+}
