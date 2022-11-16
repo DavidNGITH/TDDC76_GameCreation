4faace7eea7cb65b07cd2af2eff8435a3ebb557f
wilst185 <wilst185@tlvm-4-1-2.ad.liu.se>
2022-11-16 21:20:14 +0100

    Better helicopter img, smaller, no chemtrails

--- Helicopter.cc
+++ Helicopter.cc
@@ -8,7 +8,7 @@ Helicopter::Helicopter()
 {   
 
     //Checks if the file can be loaded
-    if (!texture.loadFromFile("helicopter.png"))
+    if (!texture.loadFromFile("helicopter1.png"))
     {
         std::cerr << "Kan inte Ã¶ppna: helkopeter.png" << std::endl;
         //return 1;
