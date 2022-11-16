47ef4bdffb74d77b71cebe5667e2c48eb397e40e
vicer124 <vicer124@student.liu.se>
2022-11-16 14:12:30 +0100

    Continius work on static object

--- static_object.h
+++ static_object.h
@@ -1,3 +1,5 @@
+#ifndef STATIC_OBJECT_H
+#define STATIC_OBJECT_H
 #include "SFML/Graphics.hpp"
 #include "game_object.h"
 
@@ -6,11 +8,22 @@ class Static_object : public Game_object
 {
     public:
 
+    Static_object();
+
     void update() override;
     void Collision() override; 
     void render() override;
     void handle() override;
     bool is_removed() override;
-    void remove() override<;
+    void remove() override;
+
+    private:
+
+    bool removed();
+
+    Sf::Sprite tree;
+    Sf::Texture icon;
 
 }
+
+#endif
