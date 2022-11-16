db78010e92dbebaaa408d9e2a42d0a78d15cfa0e
davno223 <davno223@student.liu.se>
2022-11-16 15:05:40 +0100

    Worked on handle function

--- player.cc
+++ player.cc
@@ -9,6 +9,7 @@ Player::Player()
 void Player::Aim()
 {
 
+
 }
 
 void Player::Fire()
@@ -18,6 +19,29 @@ void Player::Fire()
 
 void Player::handle(Context& context, sf::Event event)
 {
+    /*context.current_player -> handle(Context& context, sf::Event event);
+
+    //Check collsion with other
+    for (unsigned int i{0}; i < context.active_player.size(); i++)
+    {
+        for (unsigned int j{i+1}; j < context.active_player.size(); j++)
+        {
+            if context.active_player.at(i) -> collides(context.active_player.at(j))
+            {
+                context.active_player.at(i) -> collision(context.active_player.at(j));
+                context.active_player.at(j) -> collision(context.active_player.at(1));
+            }
+        }
+    }
+
+    //Check collision with other players
+    for (unsigned int i{0}; i < context.players.size(); i++)
+    {
+        if (current_player -> collides(context.players.at(i)) && current_player != context.players.at(i))
+        {
+            current_player -> collision(context.players.at(i));
+        }
+    }*/
 
 }
 
