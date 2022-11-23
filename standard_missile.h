94c4fcbcb65e992223df86bb37b0c22664a2f284
erida646 <erida646@tlvm-4-1-4.ad.liu.se>
2022-11-23 21:06:17 +0100

    Början på missil explotion

--- standard_missile.h
+++ standard_missile.h
@@ -5,15 +5,18 @@
 #include <string>
 
 
+
 class Standard_Missile : public Missile
 {
 public:
-    void Explosion();
+    void Explosion(Context& context);
 
 private:
+    sf::CircleShape explosion_2;
     std::string type{"Standard Missile"};
     int const Damage{20}; //bestämd antal hp som missilen skall skada
     int const Exlosion_radius{10}; // bestämd explosions radie
+    int i{0};
 
 
 
