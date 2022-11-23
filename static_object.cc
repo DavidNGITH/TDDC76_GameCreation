e59d01e0df70cb0194f74894a569899558082bfd
filed881 <filed881@student.liu.se>
2022-11-23 19:44:42 +0100

    Updated textures

--- static_object.cc
+++ static_object.cc
@@ -9,29 +9,29 @@
 
 Static_object::Static_object()
 {
-    load_icon("Textures/tree.png");
+    load_icon("textures_new/tree.png");
 
     position_x = rand()%(1820-100 + 1) + 100;
     position_y = 872;
 
-    icon.setScale(0.03, 0.03);
+    icon.setScale(2, 2);
     sf::Vector2u texture_size { texture.getSize() };
     icon.setOrigin(texture_size.x / 2, texture_size.y);
     icon.setPosition(position_x, position_y);    
-};
+}
 
 void Static_object::handle(Context& context, sf::Event event)
-{};
+{}
 
 void Static_object::update(Context& context)
-{};
+{}
 
 void Static_object::render(sf::RenderWindow& window, Context& context)
 {
     window.draw(icon);
-};
+}
 
 void Static_object::collision(Game_object* object, Context& context)
-{};
+{}
 
 
