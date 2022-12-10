49b4be187c6eb886b36c5818f5a679dfe01ac681
filed881 <filed881@student.liu.se>
2022-12-10 14:50:51 +0100

    Fixed seg fault with mine

--- player.cc
+++ player.cc
@@ -330,7 +330,7 @@ void Player::collision(Game_object* object, Context& context)
         {
             hp -= missile->dmg;
             std::cout << player_name_var << " - damage: " << missile -> dmg << std::endl;
-            if(missile -> this_player != this)
+            if((missile -> this_player != this) && (missile -> this_player != nullptr))
             {
                 Player* player { dynamic_cast<Player*>(missile -> this_player) };
                 player -> update_score(missile -> dmg);
